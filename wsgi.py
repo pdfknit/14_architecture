19a5a7d8d788b1f767def58ab3647c5a05714a8f
Yasenkova Sofia <yasenkovasofia@gmail.com>
2023-06-21 17:12:07 +0100

    lesson_01

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Context-Type', 'text/html')])
-    return [b'Hello, world!']
+from wsgiref.simple_server import make_server
+
+from app import Application
+from config import DEFAULT_PORT
+from urls import routes
+
+application = Application(routes, front_controller={})
+
+with make_server('', DEFAULT_PORT, application) as httpd:
+    print(f'Server running on port {DEFAULT_PORT}')
+    httpd.serve_forever()
